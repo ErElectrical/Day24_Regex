286f2f47c5cfbfaee4b2f9330c3ef4ccd41d4764
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-14 15:32:44 +0000

    [Add]Mohit Custom Exception To all validation

---
+++ CustomExceptionToAll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24_assignments
+{
+    public class CustomExceptionToAll:Exception
+    {
+        public enum ExceptionType
+        {
+            Invalid_FirstName,Invalid_Lastname,
+            Invalid_Email,Invalid_Phone,
+            Invalid_Password
+
+        }
+
+        private readonly ExceptionType exceptionType;
+
+        public CustomExceptionToAll(ExceptionType exceptionType,string message):base(message)
+        {
+            this.exceptionType=exceptionType;
+        }
+    }
+ 
+}
