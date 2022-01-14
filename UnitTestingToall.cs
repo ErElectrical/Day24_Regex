60ef61f48e5dc7f74348117ca3aacf47bb1464eb
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-14 08:05:56 +0000

    [Add]Mohit Testclass

---
+++ UnitTestingToall.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Day24_assignments
+{
+    public class UnitTestToAll
+    {
+
+        public bool ValidFirstName(string name)
+        {
+           
+            string rx = @"^[A-Z]{1}[a-z]{3,}$";
+            Regex rxName= new Regex(rx);
+            bool result = rxName.IsMatch(name);
+
+            return result;
+
+
+        }
+
+        public bool ValidLastName(string name)
+        {
+           
+            string rx = @"^[A-Z]{1}[a-z]{3,}$";
+            Regex rxName = new Regex(rx);
+            bool result = rxName.IsMatch(name);
+
+            return result;
+
+
+        }
+
+        public bool Phonenumber(string name)
+        {
+            
+            string rx = @"^ [1-9]{2,}\s[6-9][0-9]{9}$";
+            Regex rxName = new Regex(rx);
+            bool result = rxName.IsMatch(name);
+
+            return result;
+
+
+        }
+
+        public bool PassWord(string name)
+        {
+          
+            string rx = "^((?=.*[!@#$%^&*~])[A-Za-z]{8,}$";
+
+            Regex rxName = new Regex(rx);
+            bool result = rxName.IsMatch(name);
+
+            return result;
+
+
+
+
+        }
+
+
+    }
+}
